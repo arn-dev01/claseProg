d8ea1e2ef355c1142a008ab88e82c98fff0a361a
Arnulfotos <manuelleon1996@gmail.com>
2023-11-22 19:10:42 -0800

    Agrego contador de vocales

---
+++ clase12_vocales/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Ingrese una palabra");
+string palabra = Console.ReadLine()!;
+
+char[] characters = { 'a', 'e', 'i', 'o', 'u' };
+int[] charCounter = new int[characters.Length];
+
+string[] destructured = palabra.ToCharArray().Select(c => c.ToString()).ToArray();
+
+foreach (var charac in destructured)
+{
+    switch (charac)
+    {
+        case "a":
+            charCounter[0] += 1;
+            break;
+        case "e":
+            charCounter[1] += 1;
+            break;
+        case "i":
+            charCounter[2] += 1;
+            break;
+        case "o":
+            charCounter[3] += 1;
+            break;
+        case "u":
+            charCounter[4] += 1;
+            break;
+    }
+}
+
+for(int i =0; i <= destructured.Length; i++)
+{
+    Console.WriteLine($"{characters[i]} = {charCounter[i]}");
+}
