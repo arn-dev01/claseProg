ce93c0ba0ae3dba7a5bbd7c94d9348e9a2672403
arn-dev01 <149030292+arn-dev01@users.noreply.github.com>
2023-12-13 19:21:13 -0800

    Segun caso de n1 y n2 realiza operacion

---
+++ clase 3/Program.cs
@@ -0,0 +1,14 @@
+﻿int n1, n2, r;
+
+Console.WriteLine("Ingrese numero 1");
+n1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ingrese numero 2");
+n2 = int.Parse(Console.ReadLine()!);
+
+if (n1 == n2)
+    r = n1 * n2;
+else if (n1 < n2)
+    r = n1 - n2;
+else
+    r = n1 + n2;
+Console.WriteLine($"El resultado es {r}");
